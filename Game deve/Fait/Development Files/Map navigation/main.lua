89bf82eb41d42bef2144b70376125fe012322125
Teemu H <115899251+Ant1mag1c@users.noreply.github.com>
2023-09-19 11:21:08 +0300

    zc
    asd

--- Game deve/Fait/Development Files/Map navigation/main.lua
+++
@@ -1,6 +0,0 @@
-local composer = require( "composer" )
-
-display.setStatusBar( display.HiddenStatusBar )
-
-composer.gotoScene( "map", { time=500, effect="crossFade" } )
-
--- Transfer/Faith 19.9/Data/defaultStats.lua
+++
@@ -1,9 +0,0 @@
--- Pelaajan default statsit (lukkunottamatta hahmokohtaisia eroja)
-
-return {
-    money = 10,
-    maxCardsDeck = 30,
-    maxCardsHand = 5,
-    cardPerTurn = 3,
-
-}

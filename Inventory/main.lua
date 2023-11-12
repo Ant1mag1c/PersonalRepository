7ae600d7e7718c2ad7b07237b050a6598daaa9e1
Teemu H <115899251+Ant1mag1c@users.noreply.github.com>
2023-11-12 13:50:20 +0200

    Inventory V2

--- Inventory/main.lua
+++ Inventory/main.lua
@@ -4,67 +4,119 @@ local y = y or display.contentCenterY
 local obj = {}
 local itemSlot = {}
 
+
 local isInsideBounds
 
 
-local function handleItemSlots( object )
-    -- TODO: Equipped taulu täytyy luoda funktion ulkopuolella
-    itemSlot.equippedItem = {}
-    -- print(item.id)
+local function handleItemSlots( object, prevX, prevY )
+    local isFromSlot = false
+    local fromSlot
     local targetSlot
     for i = 1, #itemSlot do
         targetSlot = itemSlot[i]
-        --Tarkistetaan onko objecti irroitettu slotin sisällä
+        --Tarkistetaan onko objektista irroitettu slotin sisällä
         if object.x > targetSlot.bounds.xMin and
             object.y > targetSlot.bounds.yMin and
             object.x < targetSlot.bounds.xMax and
             object.y < targetSlot.bounds.yMax then
-                -- TODO: Jatka tästä
-            -- Tarkistetaan onko Kyseisessä slotissa jo itemiä ja jos on niin tarkistetaan
-            -- Onko target objekti tullut slotista vai uutena
-            isInsideBounds = true
+            -- TODO: Jos objekti siirtyy slotista varattuun slottiin niin voisi objektit vaihtaa
+            -- keskenään paikkaa
 
+            for i = 1, #itemSlot do
+                if itemSlot[i].equippedItem == object then
+                    isFromSlot = true
+                    fromSlot = itemSlot[i]
+                    break
+                end
+            end
+
+            isInsideBounds = true
             object.x, object.y = targetSlot.x, targetSlot.y
             break
         else
             isInsideBounds = false
-
-            if targetSlot.equippedItem then
-                -- print("Poistetaan objectin equippedItem status edelliseltä slotilta")
-                targetSlot.equippedItem = nil
-            end
         end
     end
+
+    if isFromSlot then
+        -- print( "isFromSlot ", isFromSlot, fromSlot.id )
+    end
+    -- Tarkistetaan onko valittu objekti otettu slotista
+
     -- Käsitellään itemslotin dataa
+    print("--------------------")
     if isInsideBounds then
-        if object ~= targetSlot.equippedItem then
-            if not targetSlot.equippedItem then
-                targetSlot.equippedItem = object
-                print( targetSlot.equippedItem.id .. " in slot " .. targetSlot.id )
-            else
+        if not isFromSlot then
+            if object ~= targetSlot.equippedItem then
+                if not targetSlot.equippedItem then
+                    targetSlot.equippedItem = object
+                    -- Poistetaan objektin viittaus muilta sloteilta
+                    for i = 1, #itemSlot do
+                        if itemSlot[i].equippedItem == object then
+                            if itemSlot[i] ~= targetSlot then
+                                print("Removed object from " .. itemSlot[i].id)
+                                itemSlot[i].equippedItem = nil
+                            end
+                        end
+                    end
+                    print( targetSlot.equippedItem.id .. " in slot " .. targetSlot.id )
+
                 -- Kyseisessä slotissa on jo ennestään objekti
-                print("You already have item in this slot")
-                object.x, object.y = x, y
+                else
+                    if not isFromSlot then
+                        print("You already have item in this slot")
+                        object.x, object.y = prevX, prevY
+                    -- Vaihdetaan itemien slotit keskenään
+                    else
+                        print("Swap items")
+
+                    end
+                end
+            else
+                print("This item is already in current slot")
             end
+        -- Objekti on siirretty slotilta ja taulut vaihdetaan keskenään
         else
-            print("This item is already in current slot")
-            -- object.x, object.y = originalX, originalY
+            print("Swap these two tables")
+            local _swapSlot =
+                {
+                    item1 = fromSlot.equippedItem,
+                    item2 = targetSlot.equippedItem
+
+                }
+                -- print(targetSlot.equippedItem)
+                -- print(fromSlot.equippedItem)
+            targetSlot.equippedItem = _swapSlot.item1
+            fromSlot.equippedItem = _swapSlot.item2
+
         end
     else
         print(object.id .. " is not equipped " )
-        targetSlot.equippedItem = nil
+        -- Poistetaan objektin viittaus slotilta
+        for i = 1, #itemSlot do
+            if itemSlot[i].equippedItem == object then
+                print( object.id .. " removed from slot " .. itemSlot[i].id )
+                itemSlot[i].equippedItem = nil
+                break
+            end
+        end
     end
+
+    print("-------------------")
+    print("Slot 1: ", itemSlot[1].equippedItem)
+    print("Slot 2: ", itemSlot[2].equippedItem)
+    print("Slot 3: ", itemSlot[3].equippedItem)
 end
 
+---------------------------------------------
 local originalX, originalY
 
-local function moveObject( event )
+local function moveObject( event)
     local target = event.target
     local phase = event.phase
 
     if event.phase == "began" then
-        -- Code to execute when the touch begins
-        originalX, originalY = event.x, event.y
+        originalX, originalY = target.x, target.y
         display.getCurrentStage():setFocus(target)
         target.isFocus = true
         target.xScale, target.yScale = 1.2, 1.2
@@ -80,11 +132,12 @@ local function moveObject( event )
             target.y = event.y - target.touchOffsetY
 
         else
+            -- print( "Mouse original pos: ", originalX, originalY)
             -- Objectista on irroitettu
             target.xScale, target.yScale = 1, 1
             display.getCurrentStage():setFocus(nil)
             target.isFocus = false
-            handleItemSlots( target )
+            handleItemSlots( target, originalX, originalY )
         end
     end
 
@@ -108,6 +161,7 @@ for i = 1, 3 do
     itemSlot[i]:setFillColor( 0.5 )
     itemSlot[i].bounds = itemSlot[i].contentBounds
     itemSlot[i].id = "Slot " .. i
+
 end
 -- print(itemSlot[1].bounds.xMax)
 
